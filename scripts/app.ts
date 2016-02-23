24fccd4f60e8eff15a975afdc69241df1a7dbe81
Christopher Schleiden <cschleiden@live.de>
2016-02-22 20:56:59 -0800

    Add alert action to work item context menu

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,12 +1,19 @@
 ﻿/// <reference path='../typings/tsd.d.ts' />
 
-import Q = require("q");
-
 // Register context menu action
 VSS.register("vsts-extension-ts-seed-simple-action", {
     getMenuItems: (context) => {
         return [<IContributedMenuItem>{
-            title: "Work Item Menu Action"
+            title: "Work Item Menu Action",
+            action: (actionContext) => {
+                let workItemId = actionContext.id
+                    || (actionContext.ids && actionContext.ids.length > 0 && actionContext.ids[0])
+                    || (actionContext.workItemIds && actionContext.workItemIds.length > 0 && actionContext.workItemIds[0]);
+                    
+                if (workItemId) {
+                    alert(`Selected work item ${workItemId}`);
+                }
+            }
         }];
     }
 });
