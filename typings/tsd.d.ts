9af6fd5335fdd46575958f79ce8709a8db5abcd8
Christopher Schleiden <Christopher Schleiden>
2016-03-17 22:49:04 -0700

    Remove unnecessary type reference

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="../node_modules/definition-header/dist/index.d.ts" />
 /// <reference path="../node_modules/vss-web-extension-sdk/typings/tfs.d.ts" />
 /// <reference path="../node_modules/vss-web-extension-sdk/typings/rmo.d.ts" />
 /// <reference path="../node_modules/vss-web-extension-sdk/typings/vss.d.ts" />
