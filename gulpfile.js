f6fcc8c3a05b4ebe1470268e5c12c3f8c29e1e0d
Otto Streifel <ottost@microsoft.com>
2017-06-07 22:59:18 -0700

    Use gulp

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const gulp = require('gulp');
+const ts = require("gulp-typescript");
+const clean = require("gulp-clean");
+const yargs = require("yargs");
+const exec = require('child_process').exec;
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+
+const args =  yargs.argv;
+
+const jsFolder = 'dist';
+
+const tsProject = ts.createProject('tsconfig.json', {
+    typescript: require('typescript')
+});
+gulp.task('clean', () => {
+    return gulp.src([jsFolder, '*.vsix'])
+        .pipe(clean());
+})
+
+gulp.task('build', ['clean'], () => {
+    return tsProject.src()
+        .pipe(tsProject()).js.pipe(gulp.dest(jsFolder));
+});
+
+
+gulp.task('copy', ['build'], () => {
+    gulp.src('node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js')
+        .pipe(gulp.dest(jsFolder));
+});
+
+gulp.task('package', ['copy'], () => {
+    const overrides = {}
+    if (yargs.argv.release) {
+        overrides.public = true;
+    } else {
+        const manifest = require('./vss-extension.json');
+        overrides.name = manifest.name + ": Development Edition";
+        overrides.id = manifest.id + "-dev";
+    }
+    const overridesArg = `--override "${JSON.stringify(overrides).replace(/"/g, '\\"')}"`;
+    const manifestsArg = `--manifests vss-extension.json`;
+
+    exec(`tfx extension create ${overridesArg} ${manifestsArg} --rev-version`,
+        (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+            }
+
+            console.log(stdout);
+            console.log(stderr);
+            
+        });
+
+});
+
+gulp.task('default', ['package']);
