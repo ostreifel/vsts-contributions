253f1856fc12812af626204d45974d77d3b1cf2f
Otto Streifel <ottost@microsoft.com>
2017-06-16 09:10:11 -0700

    v1.1.50 Fix filters loop updating state

--- scripts/contributionsHub.tsx
+++ scripts/contributionsHub.tsx
@@ -3,26 +3,23 @@ import { renderFilters } from "./controls/filters";
 import { renderGraph } from "./controls/graph";
 import { renderTimeWindow } from "./controls/timeWindow";
 import { updateSelectedDate } from "./controls/filters";
-import { defaultFilter, IContributionFilter, deepEqual } from "./filter";
+import { defaultFilter, IContributionFilter } from "./filter";
 import { HostNavigationService } from "VSS/SDK/Services/Navigation";
 
 VSS.getService(VSS.ServiceIds.Navigation).then(function(navigationService: HostNavigationService) {
-  let currentFilter: IContributionFilter;
   function updateHash(filter: IContributionFilter) {
-    if (!deepEqual(currentFilter, filter)) {
       const hash = encodeURI(JSON.stringify(filter));
       navigationService.setHash(hash);
-    }
   }
 
   function updateForHash(hash: string) {
     if (!hash) {
       updateHash(defaultFilter);
     }
-    currentFilter = JSON.parse(decodeURI(hash));
-    renderFilters(updateHash, currentFilter, true);
-    renderGraph(currentFilter, updateSelectedDate);
-    renderTimeWindow(currentFilter);
+    const filter = JSON.parse(decodeURI(hash));
+    renderFilters(updateHash, filter, true);
+    renderGraph(filter, updateSelectedDate);
+    renderTimeWindow(filter);
   }
   // Get current hash value from host url
   navigationService.getHash().then(updateForHash);
--- scripts/controls/filters.tsx
+++ scripts/controls/filters.tsx
@@ -3,55 +3,54 @@ import * as ReactDOM from "react-dom";
 import { CollapsibleHeader } from "./CollapsibleHeader";
 import { Toggle } from "OfficeFabric/components/toggle";
 import { IdentityPicker } from "./IdentityPicker";
-import { defaultFilter, IContributionFilter, deepEqual } from "../filter";
+import { defaultFilter, IContributionFilter } from "../filter";
 
 interface IFiltersProps {
     onChanged: (filter: IContributionFilter) => void;
-    initialFilter: IContributionFilter;
+    filter: IContributionFilter;
     collapsible?: boolean;
 }
 
 let filters: Filters;
 class Filters extends React.Component<
-  IFiltersProps,
-  IContributionFilter
+  IFiltersProps,{}
 > {
   constructor() {
     super();
-    this.state = defaultFilter;
     filters = this;
   }
   render() {
+    const { filter } = this.props;
     const collapsibleContent =
         <div className="filters">
-            <Toggle defaultChecked={this.state.allProjects} label={"All projects"} onChanged={checked => {
-                this.setState({ ...this.state, allProjects: checked });
+            <Toggle checked={filter.allProjects} label={"All projects"} onChanged={checked => {
+                this.updateFilter({ ...filter, allProjects: checked });
             }} />
-            <Toggle defaultChecked={this.state.enabledProviders.Commit} label={"Commits"} onChanged={checked => {
-                this.setState({ ...this.state, enabledProviders: { ...this.state.enabledProviders, Commit: checked } });
+            <Toggle checked={filter.enabledProviders.Commit} label={"Commits"} onChanged={checked => {
+                this.updateFilter({ ...filter, enabledProviders: { ...filter.enabledProviders, Commit: checked } });
             }} />
-            <Toggle defaultChecked={this.state.enabledProviders.CreatePullRequest} label={"Created pull requests"} onChanged={checked => {
-                this.setState({ ...this.state, enabledProviders: { ...this.state.enabledProviders, CreatePullRequest: checked } });
+            <Toggle checked={filter.enabledProviders.CreatePullRequest} label={"Created pull requests"} onChanged={checked => {
+                this.updateFilter({ ...filter, enabledProviders: { ...filter.enabledProviders, CreatePullRequest: checked } });
             }} />
-            <Toggle defaultChecked={this.state.enabledProviders.ClosePullRequest} label={"Closed pull requests"} onChanged={checked => {
-                this.setState({ ...this.state, enabledProviders: { ...this.state.enabledProviders, ClosePullRequest: checked } });
+            <Toggle checked={filter.enabledProviders.ClosePullRequest} label={"Closed pull requests"} onChanged={checked => {
+                this.updateFilter({ ...filter, enabledProviders: { ...filter.enabledProviders, ClosePullRequest: checked } });
             }} />
-            <Toggle defaultChecked={this.state.enabledProviders.CreateWorkItem} label={"Created work items"} onChanged={checked => {
-                this.setState({ ...this.state, enabledProviders: { ...this.state.enabledProviders, CreateWorkItem: checked } });
+            <Toggle checked={filter.enabledProviders.CreateWorkItem} label={"Created work items"} onChanged={checked => {
+                this.updateFilter({ ...filter, enabledProviders: { ...filter.enabledProviders, CreateWorkItem: checked } });
             }} />
-            <Toggle defaultChecked={this.state.enabledProviders.ResolveWorkItem} label={"Resolved work items"} onChanged={checked => {
-                this.setState({ ...this.state, enabledProviders: { ...this.state.enabledProviders, ResolveWorkItem: checked } });
+            <Toggle checked={filter.enabledProviders.ResolveWorkItem} label={"Resolved work items"} onChanged={checked => {
+                this.updateFilter({ ...filter, enabledProviders: { ...filter.enabledProviders, ResolveWorkItem: checked } });
             }} />
-            <Toggle defaultChecked={this.state.enabledProviders.CloseWorkItem} label={"Closed work items"} onChanged={checked => {
-                this.setState({ ...this.state, enabledProviders: { ...this.state.enabledProviders, CloseWorkItem: checked } });
+            <Toggle checked={filter.enabledProviders.CloseWorkItem} label={"Closed work items"} onChanged={checked => {
+                this.updateFilter({ ...filter, enabledProviders: { ...filter.enabledProviders, CloseWorkItem: checked } });
             }} />
         </div>;
     return (
       <div>
         <IdentityPicker
-          identity={this.state.identity}
+          identity={filter.identity}
           onIdentityChanged={identity => {
-            this.setState({ ...this.state, identity });
+            this.updateFilter({ ...filter, identity });
           }}
           width={400}
         />
@@ -67,25 +66,15 @@ class Filters extends React.Component<
       </div>
     );
   }
-  componentWillMount() {
-      this.setState(this.props.initialFilter);
-  }
-  componentWillUpdate(newProps:IFiltersProps) {
-      if (!deepEqual(this.props.initialFilter, newProps.initialFilter)) {
-          this.setState(newProps.initialFilter)
-      }
-  }
-  componentDidMount() {
-    // this.props.onChanged(this.state);
-  }
-  componentDidUpdate() {
-    this.props.onChanged(this.state);
+  updateFilter(filter: IContributionFilter) {
+    this.props.onChanged(filter);
+    renderFilters(this.props.onChanged, filter, this.props.collapsible);
   }
 }
 
 export function updateSelectedDate(date?: Date) {
-  const state: IContributionFilter = { ...filters.state, selectedDate: date };
-  filters.setState(state);
+  const filter: IContributionFilter = { ...filters.props.filter, selectedDate: date };
+  filters.updateFilter(filter);
 }
 export function getState() {
   return filters.state;
@@ -99,7 +88,7 @@ export function renderFilters(
 ) {
   const graphParent = $(".filter-container")[0];
   ReactDOM.render(
-    <Filters onChanged={onChanged} initialFilter={initialFilter} collapsible={collapsible} />,
+    <Filters onChanged={onChanged} filter={initialFilter} collapsible={collapsible} />,
     graphParent,
     callback
   );
