0286cffec798ca0784528368c14dd54eebb3feaa
Otto Streifel <ottost@microsoft.com>
2017-09-04 22:44:12 -0700

    Update changessets

--- scripts/data/tfvc/changsets.ts
+++ scripts/data/tfvc/changsets.ts
@@ -12,19 +12,34 @@ import { projects } from "../projects";
 
 const changesets: {[user: string]: {[project: string]: CachedValue<TfvcChangesetRef[]>}} = {};
 
+const batchSize = 100;
+const batchCount= 6;
 function getChangeSets(username: string, project: string, skip: number = 0): Q.IPromise<TfvcChangesetRef[]> {
-    return getClient().getChangesets(project, undefined, skip, 100).then(changesets => {
-        if (changesets.length < 100) {
-            return changesets;
-        }
-        return getChangeSets(username, project, skip + 100).then(moreChangsets => [...changesets, ...moreChangsets]);
-    }, (error: TfsError) => {
-        if (Number(error.status) === 404) {
-            return [];
-        }
+    const promises: Q.IPromise<TfvcChangesetRef[]>[] = [];
+    for (let i = 0; i < batchCount; i++) {
+        promises.push(getClient().getChangesets(project, undefined, skip + batchSize * i, batchSize));
+    }
+    return Q.all(promises).then(
+        (changesetsArr) => {
+            const changesets: TfvcChangesetRef[] = [];
+            for (const arr of changesetsArr) {
+                changesets.push(...arr);
+            }
+            if (changesets.length < batchCount * batchSize) {
+                return changesets;
+            }
+            return getChangeSets(username, project, skip + batchCount * batchSize).then(
+                (moreChangesets) => {
+                    return [...changesets, ...moreChangesets];
+                }
+            );
+        },
+        (error: TfsError) => {
+            if (Number(error.status) === 404) {
+                return [];
+            }
         return Q.reject(error)
     });
-
 }
 export class ChangsetContributionProvider implements IContributionProvider {
     public readonly name: ContributionName = "Changeset";
@@ -33,7 +48,7 @@ export class ChangsetContributionProvider implements IContributionProvider {
         const projectsPromise: Q.IPromise<string[]> = allProjects ?
             projects.getValue().then(projects => projects.map(p => p.name))
             : Q([VSS.getWebContext().project.name]);
-        return projectsPromise.then(projects => {
+        return projectsPromise.then((projects): Q.IPromise<ChangesetContribution[]> => {
             if (!(username in changesets)) {
                 changesets[username] = {}
             }
@@ -42,10 +57,10 @@ export class ChangsetContributionProvider implements IContributionProvider {
                     changesets[username][project] = new CachedValue(() => getChangeSets(username, project));
                 }
             }
-            return Q.all(projects.map(p =>
+            return Q.all(projects.map((p) =>
                 changesets[username][p].getValue().then(
-                    changesets => changesets.map(c => new ChangesetContribution(c, p))
-                ))).then(changesetsArr => {
+                    (changesets) => changesets.map(c => new ChangesetContribution(c, p))
+                ))).then((changesetsArr): ChangesetContribution[] => {
                 const changesets: ChangesetContribution[] = [];
                 for (const arr of changesetsArr) {
                     changesets.push(...arr);
