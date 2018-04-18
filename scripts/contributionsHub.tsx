18c6c9b9ebdaad4cc2480335becd7a6c78b6af2d
Otto Streifel <ottost@microsoft.com>
2018-04-17 22:42:47 -0700

    restore hash navigation

--- scripts/contributionsHub.tsx
+++ scripts/contributionsHub.tsx
@@ -1,14 +1,34 @@
 ///<reference types="vss-web-extension-sdk" />
+import { HostNavigationService } from "VSS/SDK/Services/Navigation";
+
 import { renderFilters } from "./controls/filters";
 import { renderGraphs } from "./controls/showGraphs";
 import { defaultFilter, IContributionFilter } from "./filter";
 
-async function updateGraphs(filter?: IContributionFilter)  {
-  filter = filter || await defaultFilter.getValue();
 
-  renderFilters(updateGraphs, filter, true);
-  renderGraphs(filter);
-}
-updateGraphs();
+VSS.getService<HostNavigationService>(VSS.ServiceIds.Navigation).then(async (navService) => {
+  function updateHash(filter: IContributionFilter) {
+    const hash = encodeURIComponent(JSON.stringify(filter));
+    navService.setHash(hash);
+  }
+  async function parseHash(hash: string): Promise<IContributionFilter> {
+    try {
+      return JSON.parse(decodeURIComponent(hash));
+    } catch (e) {
+      console.log("could not parse hash", hash, e);
+      return defaultFilter.getValue();
+    }
+  }
+  async function updateFromHash(hash: string) {
+    const filter = await parseHash(hash);
+
+    renderFilters(updateHash, filter, true);
+    renderGraphs(filter);
+  }
+  const hash = await navService.getHash();
+  updateFromHash(hash);
+  navService.onHashChanged(updateFromHash);
+});
+
 
 VSS.register(VSS.getContribution().id, {});
